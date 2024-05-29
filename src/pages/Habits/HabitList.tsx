e0cff91538ef09a38fc8591f0c5c551e80081ddd
Davs07 <botsymeisterspieler@gmail.com>
2024-05-28 23:48:00 -0500

    Added Ellipsis icon to HabitList CardHeader, implemented habit removal functionality, and updated HabitForm onClose prop

--- src/pages/Habits/HabitList.tsx
+++ src/pages/Habits/HabitList.tsx
@@ -3,11 +3,13 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { useHabitStore } from "@/store/habitStore";
+import { DotSquare, Ellipsis } from "lucide-react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const HabitList: React.FC = () => {
   const habits = useHabitStore((state) => state.habits);
+  const onRemoveHabit = useHabitStore((state) => state.removeHabit);
   const navigate = useNavigate();
 
   const handleRedirect = (id: string) => {
@@ -26,9 +28,16 @@ export const HabitList: React.FC = () => {
         {habits.map((habit) => (
           <Card className="space-y-0 w-[500px] py-2 rounded-2xl" key={habit.id}>
             <CardHeader
-              className="space-y-0 py-1 px-3"
+              className="space-y-0 py-1 px-3  flex-row justify-between"
               onClick={() => handleRedirect(habit.id)}>
               <div key={habit.id}>{habit.name}</div>
+              <Ellipsis
+                size={20}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onRemoveHabit(habit.id);
+                }}
+              />
             </CardHeader>
             <CardContent className="space-y-0 pt-0 pb-0 flex gap-2 px-3 text-slate-400">
               <p className="txs">{habit.category}</p>
@@ -43,7 +52,7 @@ export const HabitList: React.FC = () => {
             <Button>Agregar Hábito</Button>
           </DialogTrigger>
           <DialogContent className="p-0">
-            <HabitForm onClose={onClose}/>
+            <HabitForm onClose={onClose} />
           </DialogContent>
         </Dialog>
       </div>
